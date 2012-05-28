ec0a86cad2c7f3457205f2392bc51336b825c194
赵磊 <zhaolei-dlut@163.com>
2012-05-28 10:38:12 +0800

    完成产液量统计页面 完成故障诊断页面

--- WebContent/index.jsp
+++ WebContent/index.jsp
@@ -10,7 +10,8 @@
 
 <script type="text/javascript" charset="UTF-8"
 	src="jquery-easyui-1.2.6/jquery-1.7.1.js"></script>
-<script language="javascript" type="text/javascript" src="jqplot/excanvas.js"></script>
+<script language="javascript" type="text/javascript"
+	src="jqplot/excanvas.js"></script>
 
 <link id="index_easyuiTheme" rel="stylesheet" type="text/css"
 	href="jquery-easyui-1.2.6/themes/default/easyui.css">
@@ -20,6 +21,7 @@
 	src="jquery-easyui-1.2.6/jquery.easyui.min.js"></script>
 <script type="text/javascript" charset="UTF-8"
 	src="jquery-easyui-1.2.6/locale/easyui-lang-zh_CN.js"></script>
+<link rel="stylesheet" type="text/css" href="css/icss.css" />
 
 
 <script type="text/javascript" src="jqplot/jquery.jqplot.min.js"></script>
@@ -33,9 +35,17 @@
 	src="jqplot/plugins/jqplot.canvasAxisLabelRenderer.min.js"></script>
 <script type="text/javascript"
 	src="jqplot/plugins/jqplot.highlighter.min.js"></script>
-	
-<link rel="stylesheet" type="text/css" href="jquery-easyui-1.2.6/extension/portal.css">
-<script type="text/javascript" src="jquery-easyui-1.2.6/extension/jquery.portal.js"></script>
+<script type="text/javascript"
+	src="jqplot/plugins/jqplot.barRenderer.min.js"></script>
+<script type="text/javascript"
+	src="jqplot/plugins/jqplot.categoryAxisRenderer.min.js"></script>
+<script type="text/javascript"
+	src="jqplot/plugins/jqplot.pointLabels.min.js"></script>
+
+<link rel="stylesheet" type="text/css"
+	href="jquery-easyui-1.2.6/extension/portal.css">
+<script type="text/javascript"
+	src="jquery-easyui-1.2.6/extension/jquery.portal.js"></script>
 <!-- 	
 <script type="text/javascript" charset="UTF-8"
 	src="js/util.js"></script>   -->
--- WebContent/js/addTab.js
+++ WebContent/js/addTab.js
@@ -4,42 +4,18 @@ function addTab(title) {
 		if(typeof(tab) == 'undefined' || tab == null) {
 			$('#index_tabs').tabs('add', {
 			title : '实时示功图查询',
-			href : 'tabpanel/realtime_sgt_query.htm',
+			href : 'sgt/realtime_sgt_query.htm',
 			closable : true
 			});
 		} else {
 			$('#index_tabs').tabs('select','实时示功图查询');
 		}	
-	} else if(title == '日产量查询') {
-		var tab = $('#index_tabs').tabs('getTab','日产量查询');
-		if(typeof(tab) == 'undefined' || tab == null) {
-			$('#index_tabs').tabs('add', {
-			title : '日产量查询',
-			href : 'tabpanel/day_product_query.htm',
-			closable : true
-			});
-		} else {
-			$('#index_tabs').tabs('select','日产量查询');
-		}
-		
-	} else if(title == '时间段产量查询') {
-		var tab = $('#index_tabs').tabs('getTab','时间段产量查询');
-		if(typeof(tab) == 'undefined' || tab == null) {
-			$('#index_tabs').tabs('add', {
-			title : '时间段产量查询',
-			href : 'tabpanel/custom_day_product_query.htm',
-			closable : true
-			});
-		} else {
-			$('#index_tabs').tabs('select','时间段产量查询');
-		}
-		
 	} else if(title == '历史示功图查询') {
 		var tab = $('#index_tabs').tabs('getTab','历史示功图查询');
 		if(typeof(tab) == 'undefined' || tab == null) {
 			$('#index_tabs').tabs('add', {
 			title : '历史示功图查询',
-			href : 'tabpanel/history_sgt_query.htm',
+			href : 'sgt/history_sgt_query.htm',
 			closable : true
 			});
 		} else {
@@ -51,7 +27,7 @@ function addTab(title) {
 		if(typeof(tab) == 'undefined' || tab == null) {
 			$('#index_tabs').tabs('add', {
 			title : '多井示功图对比',
-			href : 'tabpanel/sgt_comparison_query.htm',
+			href : 'sgt/sgt_comparison_query.htm',
 			closable : true
 			});
 		} else {
@@ -63,23 +39,130 @@ function addTab(title) {
 		if(typeof(tab) == 'undefined' || tab == null) {
 			$('#index_tabs').tabs('add', {
 			title : '示功图时间对比',
-			href : 'tabpanel/sgt_comparison_singlewell_query.htm',
+			href : 'sgt/sgt_comparison_singlewell_query.htm',
 			closable : true
 			});
 		} else {
 			$('#index_tabs').tabs('select','示功图时间对比');
 		}
 		
-	} else if(title == '示功图电功图对比') {
+	}  else if(title == '日产量查询') {
+		var tab = $('#index_tabs').tabs('getTab','日产量查询');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '日产量查询',
+			href : 'oil_product/day_product_query.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','日产量查询');
+		}
+		
+	} else if(title == '时间段产量查询') {
+		var tab = $('#index_tabs').tabs('getTab','时间段产量查询');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '时间段产量查询',
+			href : 'oil_product/custom_day_product_query.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','时间段产量查询');
+		}
+		
+	}else if(title == '示功图电功图对比') {
 		var tab = $('#index_tabs').tabs('getTab','示功图电功图对比');
 		if(typeof(tab) == 'undefined' || tab == null) {
 			$('#index_tabs').tabs('add', {
 			title : '示功图电功图对比',
-			href : 'tabpanel/sgt_dgt_comparison_query.htm',
+			href : 'sgt_dgt/sgt_dgt_comparison_query.htm',
 			closable : true
 			});
 		} else {
 			$('#index_tabs').tabs('select','示功图电功图对比');
+		}
+		
+	} else if(title == '实时电功图查询') {
+		var tab = $('#index_tabs').tabs('getTab','实时电功图查询');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '实时电功图查询',
+			href : 'dgt/realtime_dgt_query.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','实时电功图查询');
+		}	
+	} else if(title == '历史电功图查询') {
+		var tab = $('#index_tabs').tabs('getTab','历史电功图查询');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '历史电功图查询',
+			href : 'dgt/history_dgt_query.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','历史电功图查询');
+		}
+		
+	} else if(title == '多井电功图对比') {
+		var tab = $('#index_tabs').tabs('getTab','多井电功图对比');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '多井电功图对比',
+			href : 'dgt/dgt_comparison_query.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','多井电功图对比');
+		}
+		
+	} else if(title == '电功图时间对比') {
+		var tab = $('#index_tabs').tabs('getTab','电功图时间对比');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '电功图时间对比',
+			href : 'dgt/dgt_comparison_singlewell_query.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','电功图时间对比');
+		}
+		
+	} else if(title == '井矿诊断') {
+		var tab = $('#index_tabs').tabs('getTab','井矿诊断');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '井矿诊断',
+			href : 'sys/gzzd.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','井矿诊断');
+		}
+		
+	} else if(title == '月产量统计') {
+		var tab = $('#index_tabs').tabs('getTab','月产量统计');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '月产量统计',
+			href : 'oil_product/product_month.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','月产量统计');
+		}
+		
+	} else if(title == '年产量统计') {
+		var tab = $('#index_tabs').tabs('getTab','年产量统计');
+		if(typeof(tab) == 'undefined' || tab == null) {
+			$('#index_tabs').tabs('add', {
+			title : '年产量统计',
+			href : 'oil_product/product_year.htm',
+			closable : true
+			});
+		} else {
+			$('#index_tabs').tabs('select','年产量统计');
 		}
 		
 	}
